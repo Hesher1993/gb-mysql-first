4bbf149a765e83767458b2a9a1d846a7bf6b22f7
ofadeev <ofadeev@maxlevel.ru>
2018-04-28 13:15:33 +0300

    hw3

--- hw3/hw3.sql
+++ hw3/hw3.sql
@@ -1,10 +1,13 @@
 USE 'employees';
 -- База данных «Страны и города мира».
 -- 1. Сделать запрос, в котором мы выберем все данные о городе – регион, страна.
-SELECT _cities.title as city_name, _regions.title as region_name, _countries.title as country_name
-FROM _cities
-JOIN _regions ON _cities.region_id = _regions.id
-JOIN _countries ON _cities.country_id = _countries.id
+SELECT
+  ci.title as city_name,
+  r.title as region_name,
+  co.title as country_name
+FROM _cities ci
+  JOIN _regions r ON ci.region_id = r.id
+JOIN _countries co ON ci.country_id = co.id
 LIMIT 10;
 -- +--------------+-----------------------+-----------+
 -- | title        | title                 | title     |
@@ -23,10 +26,10 @@ LIMIT 10;
 -- 10 rows in set (0.00 sec)
 
 -- 2. Выбрать все города из Московской области.
-SELECT _regions.title as region_name, _cities.title as city_name
-FROM _regions
-JOIN _cities ON _regions.id = _cities.region_id
-WHERE _regions.title = 'Московская область'
+SELECT r.title as region_name, c.title as city_name
+FROM _regions r
+JOIN _cities ci ON r.id = ci.region_id
+WHERE r.title = 'Московская область'
 LIMIT 5;
 -- +--------------------+-----------+
 -- | region_name        | city_name |
@@ -51,23 +54,22 @@ FROM departments dp
   JOIN dept_emp de ON dp.dept_no = de.dept_no
   JOIN salaries s ON de.emp_no = s.emp_no
 GROUP BY dp.dept_name;
--- +--------------------+----------------------+
--- | dept_name          | AVG(salaries.salary) |
--- +--------------------+----------------------+
--- | Customer Service   |           58770.3665 |
--- | Development        |           59478.9012 |
--- | Finance            |           70489.3649 |
--- | Human Resources    |           55574.8794 |
--- | Marketing          |           71913.2000 |
--- | Production         |           59605.4825 |
--- | Quality Management |           57251.2719 |
--- | Research           |           59665.1817 |
--- | Sales              |           80667.6058 |
--- +--------------------+----------------------+
--- 9 rows in set (1 min 20.54 sec)
+-- +--------------------+------------+
+-- | dept_name          | avg_salary |
+-- +--------------------+------------+
+-- | Customer Service   | 58770.3665 |
+-- | Development        | 59478.9012 |
+-- | Finance            | 70489.3649 |
+-- | Human Resources    | 55574.8794 |
+-- | Marketing          | 71913.2000 |
+-- | Production         | 59605.4825 |
+-- | Quality Management | 57251.2719 |
+-- | Research           | 59665.1817 |
+-- | Sales              | 80667.6058 |
+-- +--------------------+------------+
+-- 9 rows in set (2.53 sec)
 
 -- 2. Выбрать максимальную зарплату у сотрудника.
--- Максимальная зарплата у всех сотрудников
 SELECT
   e.emp_no,
   CONCAT(e.first_name, ' ', e.last_name) as name,
@@ -91,15 +93,15 @@ DESC LIMIT 10;
 -- | 266526 | Weijing Chenoweth |     152710 |
 -- | 237542 | Weicheng Hatcliff |     152687 |
 -- +--------+-------------------+------------+
--- 10 rows in set (3.55 sec)
--- Максимальная зарплата у одного сотрудника
+-- 10 rows in set (1.93 sec)
+
+-- 2.1 Максимальная зарплата у одного сотрудника
 SELECT
   e.emp_no,
   CONCAT(e.first_name, ' ', e.last_name) as name,
   MAX(s.salary) as max_salary
 FROM employees e
   JOIN salaries s ON e.emp_no = s.emp_no
-WHERE e.emp_no = 43624
 GROUP BY e.emp_no
 ORDER BY max_salary DESC
 LIMIT 1;
@@ -111,15 +113,75 @@ LIMIT 1;
 -- 1 row in set (0.00 sec)
 
 -- 3. Удалить одного сотрудника, у которого максимальная зарплата.
+DELETE FROM employees
+WHERE emp_no = (
+  SELECT s.emp_no
+  FROM salaries s
+  WHERE s.salary = (
+    SELECT MAX(salary)
+    FROM salaries
+  )
+  LIMIT 1
+);
+
 -- 4. Посчитать количество сотрудников во всех отделах.
+SELECT
+  dp.dept_name,
+  COUNT(de.emp_no) as count
+FROM departments dp
+  JOIN dept_emp de ON de.dept_no = dp.dept_no
+GROUP BY dp.dept_no;
+-- +--------------------+-------+
+-- | dept_name          | count |
+-- +--------------------+-------+
+-- | Marketing          | 20211 |
+-- | Finance            | 17346 |
+-- | Human Resources    | 17786 |
+-- | Production         | 73485 |
+-- | Development        | 85707 |
+-- | Quality Management | 20117 |
+-- | Sales              | 52245 |
+-- | Research           | 21126 |
+-- | Customer Service   | 23580 |
+-- +--------------------+-------+
+-- 9 rows in set (0.14 sec)
+
+-- 4.2 По настоящее время
+SELECT
+  dp.dept_name,
+  COUNT(de.emp_no) as count
+FROM departments dp
+  JOIN dept_emp de
+    ON de.dept_no = dp.dept_no
+    AND de.to_date > NOW()
+GROUP BY dp.dept_no;
+-- +--------------------+-------+
+-- | dept_name          | count |
+-- +--------------------+-------+
+-- | Marketing          | 14842 |
+-- | Finance            | 12437 |
+-- | Human Resources    | 12898 |
+-- | Production         | 53304 |
+-- | Development        | 61386 |
+-- | Quality Management | 14546 |
+-- | Sales              | 37701 |
+-- | Research           | 15441 |
+-- | Customer Service   | 17569 |
+-- +--------------------+-------+
+-- 9 rows in set (0.66 sec)
+
+
 -- 5. Найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел.
-SELECT departments.dept_name as dept_name, COUNT(*) as count_salary, SUM(salaries.salary) as max_salary
-FROM departments
-JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
-JOIN employees ON employees.emp_no = dept_emp.emp_no
-JOIN salaries ON employees.emp_no = salaries.emp_no
-GROUP BY departments.dept_no
-ORDER BY max_salary;
+SELECT
+  dp.dept_name,
+  COUNT(de.emp_no) as count,
+  SUM(s.salary) as sum_salary
+FROM departments dp
+  JOIN dept_emp de
+    ON de.dept_no = dp.dept_no
+  JOIN salaries s
+    ON de.emp_no = s.emp_no
+GROUP BY dp.dept_no;
 -- +--------------------+--------------+-------------+
 -- | dept_name          | count_salary | max_salary  |
 -- +--------------------+--------------+-------------+
